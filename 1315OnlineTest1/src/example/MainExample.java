a525ce27391e537504c2146896ecfae7bb5ee3bf
Arbaaz <Arbaaz@ABZONIA>
2015-07-09 14:14:03 +0530

    ver4

---
+++ 1315OnlineTest1/src/example/MainExample.java
@@ -0,0 +1,11 @@
+package example;
+
+public class MainExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Newexample n = new Newexample();
+		n.getvalues();
+	}
+
+}
